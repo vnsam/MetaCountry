3ccda2fed1d3715abdad871eee45e3fda3181a35
Vignesh, Narayanasamy <vigneshn@thoughtworks.com>
2019-04-16 04:13:28 +0530

    Adds model - country

---
+++ MetaCountry/MetaCountry/Scene/Home/Model/Country.swift
@@ -0,0 +1,28 @@
+//
+//  Country.swift
+//  MetaCountry
+//
+//  Created by Vignesh Narayanasamy on 16/04/19.
+//  Copyright © 2019 Vignesh Narayanasamy. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Country {
+    private(set) var countryCode: String
+    private(set) var countryName: String
+    private(set) var currencyCode: String
+
+    init?(value: [String: AnyObject]) {
+        guard
+            let countryCode = value["countryCode"] as? String,
+            let countryName = value["countryName"] as? String,
+            let currencyCode = value["currencyCode"] as? String else {
+                return nil
+        }
+        self.countryCode = countryCode
+        self.countryName = countryName
+        self.currencyCode = currencyCode
+    }
+}
