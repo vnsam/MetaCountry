b0c947d9b7b6fddb4bc91bae05f0dff8a93841af
Vignesh, Narayanasamy <vigneshn@thoughtworks.com>
2019-04-16 03:19:33 +0530

    Reorgs project directory. Boilerplate code cleanup

---
+++ MetaCountry/MetaCountry/ApplicationDelegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MetaCountry
+//
+//  Created by Vignesh Narayanasamy on 16/04/19.
+//  Copyright © 2019 Vignesh Narayanasamy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
